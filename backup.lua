ed993c93dd81147e080e936a80d60ecd96226906
MikaellaChry <mika.305@hotmail.com>
2016-01-28 09:24:25 +0200

    Update backup.lua

--- backup.lua
+++ backup.lua
@@ -1,6 +1,6 @@
 MODE = 'history' -- 'history' or 'users'
-CONTACT_NAME = 'Raff' -- It can be a part of contacts name
-MESSAGE_COUNT = 500
+CONTACT_NAME = 'Mika' -- It can be a part of contacts name
+MESSAGE_COUNT = 1500
 TYPE = 'csv' -- 'csv' or 'sqlite'
 DATABASE_FILE = 'db.sqlite3'
 CSV_FILE_CONTACTS = 'contacts.csv'
